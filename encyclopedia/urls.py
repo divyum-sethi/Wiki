0e6fc5733f93366d473def0676e9c924d4d96bb0
unknown <divyumsethi9@gmail.com>
2020-09-15 16:59:16 +0530

    1.2

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,7 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("wiki/<str:title>", views.title, name="title"),
+
     path("wiki/search/", views.search, name='search'),
+    path("wiki/create/", views.create, name="new"),
+     path("wiki/edit/<str:title>", views.edit, name="edit"),
+    path("wiki/random/", views.ran, name="ran"),
+    path("wiki/<str:title>", views.title, name="title"),
     path("wiki/", views.index, name="index")
+
+
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-
+from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from . import util
 import markdown2
+import random
+
+class NewEntry(forms.Form):
+    title = forms.CharField(required=True,label="title")
+    content = forms.CharField(label="content",widget=forms.Textarea(attrs={'cols': 5, 'rows': 2}))
 
 
 
@@ -17,7 +24,7 @@ def title(request, title):
             "content": markdown2.markdown(util.get_entry(title)), "title":title
         })
     else:
-        return render(request, "encyclopedia/error.html")
+        return render(request, "encyclopedia/error.html", {"message": "No such entry found"})
 
 def search(request):
     if request.method=="POST":
@@ -36,3 +43,54 @@ def search(request):
             return render(request, "encyclopedia/index.html" , {
                 "entries" : lst
             })
+
+def create(request):
+    if request.method=='POST':
+        form = NewEntry(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            content = form.cleaned_data['content']
+            entries = util.list_entries()
+            if title in entries:
+                return render(request, "encyclopedia/error.html", {"message": "Page already exist"})
+            else:
+                util.save_entry(title,content)
+                return render(request, "encyclopedia/content.html", {
+                "content": content, "title" : title
+                })
+
+    return render(request, "encyclopedia/new.html", {
+        "form" : NewEntry()
+    })
+
+def edit(request, title):
+    #title is passed as we wrote <str:title> in url
+    #title is passed as we wrote title with the url in edit.html
+    if request.method == 'GET':
+        page = util.get_entry(title)
+        form = NewEntry(initial = {'content': page, 'title':title})
+        return render(request,"encyclopedia/edit.html", {
+        "form":form })
+
+    else:
+        NewEntry(request.POST)
+
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            content = form.cleaned_data['content']
+            util.save_entry(title,content)
+            content = markdown2.markdown(content)
+            return render(request, "encyclopedia/content.html", {
+            "content": content, "title" : title
+            })
+
+
+def ran(request):
+    entries = util.list_entries()
+    size = len(entries)
+    get = random.randint(0,size-1)
+    entry = entries[get]
+    content = markdown2.markdown(util.get_entry(entry))
+    return render(request, "encyclopedia/content.html", {
+    "content": content, "title" : entry
+    })
